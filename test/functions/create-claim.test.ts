3232809e211234dbae24ba7a5d88ed9a775a78bc
Oliver Wilson <88714299+woolinsilver-sknups@users.noreply.github.com>
2023-04-13 15:05:29 +0100

    PLATFORM-3739 Add special case for Unrestricted Drop Links with identifier 0

--- test/functions/create-claim.test.ts
+++ test/functions/create-claim.test.ts
@@ -198,7 +198,7 @@ describe('function - create-claim', () => {
       expect(mocks.itemClient.createItem).toHaveBeenCalledTimes(1);
       expect(mocks.datastoreHelper.saveEntity).toHaveBeenLastCalledWith(
         'claim',
-        _claimEntity({ dropLinkIdentifier: 'unrestricted-0' }),
+        _claimEntity({ dropLinkIdentifier: 'unrestricted' }),
       );
       expect(mocks.datastoreHelper.saveEntity).toHaveBeenCalledTimes(1);
     });
